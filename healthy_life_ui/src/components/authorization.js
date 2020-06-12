da8ac591be6fd5233746d3079ffa618e2208689d
Владислав Ковалёк <vlad.kovalyok.99@gmail.com>
2020-06-12 15:05:07 +0300

    some changes

--- healthy_life_ui/src/components/authorization.js
+++ healthy_life_ui/src/components/authorization.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 class Authorization extends React.Component {
     state = {login: "", password: ""}
--- healthy_life_ui/src/components/users.js
+++ healthy_life_ui/src/components/users.js
@@ -13,7 +13,7 @@ class Users extends React.Component{
         const data =  await response.json();
         
         data.map(item => {
-          const obj = {id: item.id, firstName: item.firstName, lastName: item.lastName, username: item.username, height: item.height, weight: item.weight}
+          const obj = {id: item.id, firstName: item.firstName, lastName: item.lastName, username: item.username, email: item.email}
           this.setState({
            users: [...this.state.users, obj]
           })
@@ -42,21 +42,46 @@ render() {
   }
 
 return(
-<div>
-    <h2>Список пользователей</h2>
+<div className="users-body">
 <div>
     {
-        this.state.users.map(item => <div key={item.id} className='list'>
-         <div className="elem">id: {item.id}</div> 
-         <div className="elem">Имя пользователя: {item.username}</div> 
-         <div className="elem">Имя: {item.firstName}</div> 
-         <div className="elem">Фамилия: {item.lastName}</div>   
-         <div className="elem">Рост: {item.height}см</div> 
-         <div className="elem">Вес: {item.weight}кг</div> 
-         <Button variant="outline-success" className ='update-button'>Изменить</Button>
-         <Button variant="outline-danger" className ='delete-button' id={item.id} onClick={this.deleteUser}>Удалить</Button>
-        </div>
-        )
+        <table className="users-table">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>username</th>
+              <th>firstname</th>
+              <th>lastname</th>
+              <th>email</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.users.map(item => 
+        <tr key={item.id}>
+          <td>{item.id}</td>
+          <td>{item.username}</td>
+          <td>{item.firstName}</td>
+          <td>{item.lastName}</td>
+          <td>{item.email}</td>
+          <button>Подробнее</button>
+          <button>Удалить</button>
+        </tr>
+  )}
+          </tbody>
+        </table>
+
+        // this.state.users.map(item => 
+        // <div key={item.id} className='list'>
+        //  <div className="elem">id: {item.id}</div> 
+        //  <div className="elem">Имя пользователя: {item.username}</div> 
+        //  <div className="elem">Имя: {item.firstName}</div> 
+        //  <div className="elem">Фамилия: {item.lastName}</div>   
+        //  <div className="elem">Рост: {item.height}см</div> 
+        //  <div className="elem">Вес: {item.weight}кг</div> 
+        //  <Button variant="outline-success" className ='update-button'>Изменить</Button>
+        //  <Button variant="outline-danger" className ='delete-button' id={item.id} onClick={this.deleteUser}>Удалить</Button>
+        // </div>
+        // )
     }
 </div>
 </div>
